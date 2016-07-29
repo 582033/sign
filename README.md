8d07b7189a86c441e524895df4bfcd42f2cb04f3
Yan Jiang <j582033@163.com>
2016-07-29 08:56:50 +0800

    demo init

---
+++ README.md
@@ -0,0 +1,9 @@
+###基于cookie的简易签到
+
+> 可配合crontab进行京东自动领金币,贴吧自动签到等
+
+1. chrome安装[EditThisCooike](https://chrome.google.com/webstore/detail/editthiscookie/fngmhnnpilhplaeedifhccceomclgfbg/reviews)插件
+2. 使用 EditThisCooike 复制cookie到粘贴板 ![](http://ww3.sinaimg.cn/large/633e0588jw1f6ahc8i0mnj20um0yy42o.jpg)
+3. 把复制的cookie粘贴到目录 cookie/<site-name>.json, 例如: cookie/tieba.json
+4. 修改sign.py文件,为字典 sign_dict 增加元素 ![](http://ww4.sinaimg.cn/large/633e0588jw1f6ahis8g1kj20n1081dgn.jpg)
+
---
+++ sign.py
@@ -0,0 +1,69 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import requests
+import json
+import pdb
+from bs4 import BeautifulSoup
+from pprint import pprint
+
+
+class Sign:
+    def __init__(self, cookie_path, sign_dict):
+        self.cookie_path = cookie_path
+        self.sign_dict = sign_dict
+
+    #@site : sign_dict key
+    #@info : sign_dict value
+    def request(self, site, info):
+        #读取cookie
+        cookie_file = "%s%s.json" % (self.cookie_path, site)
+
+        #断点调试
+        #pdb.set_trace()
+
+        with open(cookie_file) as json_data:
+            cookie_list = json.load(json_data)
+
+        #cookie存入dict
+        cookie = dict()
+        for arr in cookie_list:
+            cookie[arr['name']] = arr['value']
+
+        #进行请求
+        form_data = info['form'] if 'form' in info else {}
+
+        if (info['method'] == 'get'):
+            r = requests.get(info['url'], cookies=cookie)
+        if (info['method'] == 'post'):
+            r = requests.post(info['url'], cookies=cookie, data=form_data)
+        soup = BeautifulSoup(r.content, 'html.parser')
+        return soup
+
+    def sign_all(self):
+        for site in sign_dict:
+            log = self.request(site, sign_dict[site])
+            print("[%s] ==  %s" % (site, log))
+
+
+
+if __name__ == '__main__':
+    cookie_path = '/root/sign/cookie/'
+    sign_dict = {
+        'jd' : {
+            'url'   : 'http://vip.jd.com/index.php?mod=Vip.Ajax&action=signIn&callback=jQuery4415515&_=1469579847791',
+            'method' : 'get',
+        },
+        'tieba' : {
+            'url'   : 'http://tieba.baidu.com/sign/add',
+            'method' : 'post',
+            'form' : {
+                    'ie' : 'utf-8',
+                    'kw' : '李毅',
+                    'tbs' : 'ae7fce58040c07061469429777'
+            }
+        }
+    }
+
+    sign = Sign(cookie_path, sign_dict)
+    sign.sign_all()
